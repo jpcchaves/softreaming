24b564bd060887ef4692597003046581ea1e3031
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-09-25 20:00:02 -0300

    changed the themeProvider to App.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EnterPage from "./pages/homePage";
 import AuthPage from "./pages/authPage";
 import SignUpPage from "./pages/sign-up";
+// styled components
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 
 const App: React.FC = () => {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<EnterPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/" element={<EnterPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Routes>
+        </ThemeProvider>
         <GlobalStyles />
       </Router>
     </>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// styled components
-import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
