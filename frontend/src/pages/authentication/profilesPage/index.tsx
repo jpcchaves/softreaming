5362c85a373bb2936bf3b876803af7ee93672632
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-10-03 23:00:39 -0300

    created profilesPage structure and styling

--- frontend/src/pages/authentication/profilesPage/index.tsx
+++ frontend/src/pages/authentication/profilesPage/index.tsx
@@ -1,17 +1,72 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../../contexts/auth/AuthContext";
 // styled
-import { ProfilesPageWrapper } from "./style";
+import {
+  Logo,
+  LogoContainer,
+  LogoWrapper,
+  ManageProfilesButton,
+  ManageProfilesButtonWrapper,
+  ProfileBanner,
+  ProfileImage,
+  ProfileImageWrapper,
+  ProfileName,
+  ProfilesButtonText,
+  ProfilesContainer,
+  ProfilesPageWrapper,
+  ProfilesTitle,
+  ProfilesTitleContainer,
+  ProfilesWrapper,
+} from "./style";
+// Logo
+import LogoImage from "../../../assets/logo/logo.png";
 
 const ProfilesPage: React.FC = () => {
-
-  const auth = useContext(AuthContext)
+  const auth = useContext(AuthContext);
 
   return (
-    <ProfilesPageWrapper style={{ color: "white", textAlign: 'center', marginTop: '10rem' } }>
-      {auth.user?.email}    
-      {auth.user?.id}    
-      {auth.user?.userName}    
+    <ProfilesPageWrapper>
+      <LogoContainer>
+        <LogoWrapper>
+          <Logo src={LogoImage} alt="logotipo da softreaming" />
+        </LogoWrapper>
+      </LogoContainer>
+      <ProfilesContainer>
+        <ProfilesTitleContainer>
+          <ProfilesTitle>Quem está assistindo?</ProfilesTitle>
+        </ProfilesTitleContainer>
+        <ProfilesWrapper>
+          <ProfileBanner>
+            <ProfileImageWrapper>
+              <ProfileImage src="https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png" />
+              <ProfileName>Profile Name</ProfileName>
+            </ProfileImageWrapper>
+          </ProfileBanner>
+          <ProfileBanner>
+            <ProfileImageWrapper>
+              <ProfileImage src="https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png" />
+              <ProfileName>Profile Name</ProfileName>
+            </ProfileImageWrapper>
+          </ProfileBanner>
+          <ProfileBanner>
+            <ProfileImageWrapper>
+              <ProfileImage src="https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png" />
+              <ProfileName>Profile Name</ProfileName>
+            </ProfileImageWrapper>
+          </ProfileBanner>
+          <ProfileBanner>
+            <ProfileImageWrapper>
+              <ProfileImage src="https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png" />
+              <ProfileName>Profile Name</ProfileName>
+            </ProfileImageWrapper>
+          </ProfileBanner>
+        </ProfilesWrapper>
+        <ManageProfilesButtonWrapper>
+          <ManageProfilesButton>
+            <ProfilesButtonText>Gerenciar perfis</ProfilesButtonText>
+          </ManageProfilesButton>
+        </ManageProfilesButtonWrapper>
+      </ProfilesContainer>
     </ProfilesPageWrapper>
   );
 };
--- frontend/src/pages/authentication/profilesPage/style.ts
+++ frontend/src/pages/authentication/profilesPage/style.ts
@@ -1,6 +1,97 @@
-import styled from "styled-components";
+import styled, { ThemeContext } from "styled-components";
 
 export const ProfilesPageWrapper = styled.div`
   width: 100vw;
-  height: 100vh - 80px;
+  height: 100vh;
+`;
+
+export const LogoContainer = styled.div`
+  height: 85px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const LogoWrapper = styled.div``;
+
+export const Logo = styled.img`
+  height: 2.8125rem;
+  width: 10.4375rem;
+  margin-left: 60px;
+`;
+
+export const ProfilesContainer = styled.div`
+  width: 100%;
+  padding: 0 50px;
+  height: calc(100vh - 85px);
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProfilesTitleContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  margin: 4rem 0 2rem 0;
+`;
+
+export const ProfilesTitle = styled.h1`
+  text-align: center;
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: ${({ theme }) => theme.fonts.weight.medium};
+  font-size: ${({ theme }) => theme.fonts.size.extraBig};
+  text-shadow: 1px 1px 1px black;
+`;
+
+export const ProfilesWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+  flex-wrap: wrap;
+  width: 100%;
+`;
+
+export const ProfileBanner = styled.div`
+  width: 200px;
+  height: 253px;
+  text-align: center;
+`;
+
+export const ProfileImageWrapper = styled.div`
+  width: 100%;
+  height: 200px;
+`;
+
+export const ProfileImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const ProfileName = styled.p`
+  color: ${({ theme }) => theme.colors.midGray};
+  font-size: ${({ theme }) => theme.fonts.size.mediumBigSize};
+  margin: .5rem 0;
+`;
+
+export const ManageProfilesButtonWrapper = styled.div`
+  width: 100%;
+  padding: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ManageProfilesButton = styled.button`
+  background-color: transparent;
+  border: 1px solid #6d6d6e;
+  padding: 10px 5px;
+  border-radius: 5px;
+  width: 400px;
+`;
+
+export const ProfilesButtonText = styled.span`
+  text-transform: uppercase;
+  font-size: ${({ theme }) => theme.fonts.size.mediumBigSize};
+  color: ${({ theme }) => theme.colors.midGray};
 `;
