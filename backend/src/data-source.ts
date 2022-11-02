1f02de70c88eaeaef8e334b03b09300ae0f874e8
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-11-02 20:37:39 -0300

    fixed node types to tsconfig.json also removed unused code in data-souce.ts

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -2,8 +2,6 @@ import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-const port = process.env.DB_PORT as number | undefined;
-
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DB_URI,
