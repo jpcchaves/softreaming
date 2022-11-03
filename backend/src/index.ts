f40ffa725bc79ca3e2923e811a3dbb3c1c53bc80
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-11-02 21:37:55 -0300

    added incremental to true into tsconfing.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,13 +13,6 @@ AppDataSource.initialize().then(() => {
   const app = express();
   const port = process.env.PORT;
 
-  app.use(
-    cors({
-      origin: "*",
-      credentials: true,
-    })
-  );
-
   const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
@@ -30,6 +23,12 @@ AppDataSource.initialize().then(() => {
   app.use(express.json());
 
   app.use(routes);
+
+  app.use(
+    cors({
+      credentials: true,
+    })
+  );
 
   return app.listen(port, () => {
     `API running on port ${port}`;
