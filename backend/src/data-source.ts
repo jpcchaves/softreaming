2304740eb5df9c9e627a5cafe5de8e1e3ddd186f
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-11-03 22:51:46 -0300

    updated entities path into data-source folder

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -5,6 +5,6 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DB_URI,
-  entities: [`${__dirname}/**/entities/*.{ts, js}`],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [`${__dirname}/**/migrations/*.{ts, js}`],
 });
