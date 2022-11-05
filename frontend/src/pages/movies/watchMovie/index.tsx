a314ef3a638ccaf9d87c2520562c717e1340dcd0
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-11-04 22:28:35 -0300

    added loading state to watch movie page

--- frontend/src/pages/movies/watchMovie/index.tsx
+++ frontend/src/pages/movies/watchMovie/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import LoadingSpan from "../../../components/loadingSpan";
 import { api } from "../../../hooks/useApi";
 import { Movie } from "../../../types/Movie";
 import {
@@ -16,6 +17,7 @@ import {
 const WatchMovie = () => {
   const { movieId } = useParams();
   const [movieBeingWatched, setMovieBeingWatcher] = useState<Movie>();
+  const [isLoading, setIsLoading] = useState(true);
 
   const getToken = () => {
     const token = localStorage.getItem("authToken");
@@ -33,8 +35,10 @@ const WatchMovie = () => {
           },
         });
         setMovieBeingWatcher(movie.data);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
+        setIsLoading(false);
       }
     };
     fetchMovieBeingWatched();
@@ -42,28 +46,35 @@ const WatchMovie = () => {
 
   return (
     <WatchMoviePageContainer>
-      <MovieDetailsWrapper>
-        <MovieDetails>
-          <MovieTitle>
-            {movieBeingWatched?.movieName} ({movieBeingWatched?.releaseDate})
-          </MovieTitle>
-          <MovieCategory>
-            {movieBeingWatched?.category}, {movieBeingWatched?.duration} min
-          </MovieCategory>
-          <MovieInfo>{movieBeingWatched?.description}</MovieInfo>
-          <MovieInfo></MovieInfo>
-        </MovieDetails>
-      </MovieDetailsWrapper>
-      <MovieContainer>
-        <YoutubeIframe
-          allowFullScreen
-          width="560"
-          height="315"
-          src={movieBeingWatched?.movie_url}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; gyroscope; picture-in-picture"
-        ></YoutubeIframe>
-      </MovieContainer>
+      {isLoading ? (
+        <LoadingSpan />
+      ) : (
+        <>
+          <MovieDetailsWrapper>
+            <MovieDetails>
+              <MovieTitle>
+                {movieBeingWatched?.movieName} ({movieBeingWatched?.releaseDate}
+                )
+              </MovieTitle>
+              <MovieCategory>
+                {movieBeingWatched?.category}, {movieBeingWatched?.duration} min
+              </MovieCategory>
+              <MovieInfo>{movieBeingWatched?.description}</MovieInfo>
+              <MovieInfo></MovieInfo>
+            </MovieDetails>
+          </MovieDetailsWrapper>
+          <MovieContainer>
+            <YoutubeIframe
+              allowFullScreen
+              width="560"
+              height="315"
+              src={movieBeingWatched?.movie_url}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; gyroscope; picture-in-picture"
+            ></YoutubeIframe>
+          </MovieContainer>
+        </>
+      )}
     </WatchMoviePageContainer>
   );
 };
