406837756e6ca0097c966e9ad23ef1b4e0fc0717
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-05 18:46:15 -0300

    updated cors policy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,6 +18,8 @@ AppDataSource.initialize().then(() => {
   const app = express();
   const port = process.env.PORT;
 
+  app.use(cors({origin: 'https://softreaming.netlify.app',credentials: true}));
+
   app.locals.urlProfileS3;
 
   app.use(express.json());
@@ -34,7 +36,6 @@ AppDataSource.initialize().then(() => {
 
   app.use('/', limiter);
 
-  app.use(cors({origin: 'https://softreaming.netlify.app',credentials: true}));
 
   app.use(errorMiddleware);
 
