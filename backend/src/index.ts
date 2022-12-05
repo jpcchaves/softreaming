9a1d3c273bb28804e5e6dbb71a358cde78ecf736
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-05 18:30:55 -0300

    updated cors policy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,6 +24,14 @@ AppDataSource.initialize().then(() => {
     origin: '*'
   }));
 
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
+
   app.use(express.json());
 
   app.use('/user', userRoute);
